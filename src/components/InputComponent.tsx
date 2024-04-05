2bfe42947650ce13130b98b2a5c2643aa7797404
dvch162 <deefordis8@gmail.com>
2024-04-05 10:48:09 +0300

    Create InputComponent

---
+++ src/components/InputComponent.tsx
@@ -0,0 +1,28 @@
+import React, { forwardRef } from 'react';
+
+interface InputComponentProps {
+  value: string;
+  onChange: (value: string) => void;
+  onFocus: () => void;
+}
+
+    const InputComponent: React.ForwardRefRenderFunction<HTMLInputElement, InputComponentProps> = ({ value, onChange, onFocus }, ref) => {
+  return (
+    <input
+      ref={ref}
+      value={value}
+      type="text"
+      className="base_input"
+      aria-autocomplete="list"
+      aria-expanded="true"
+      onChange={(e) => onChange(e.target.value)}
+      onFocus={onFocus}
+    />
+  );
+};
+
+
+const ForwardedInputComponent = forwardRef(InputComponent);
+ForwardedInputComponent.displayName = 'InputComponent'; 
+
+export default ForwardedInputComponent;
