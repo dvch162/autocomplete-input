5473372fafcaf64b03b10b307c441e471eaa844f
dvch162 <deefordis8@gmail.com>
2024-04-05 13:31:51 +0300

    Create DropdownComponent

---
+++ src/components/DropdownComponent.tsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import { AutocompleteItem } from "../App";
+
+interface DropdownComponentProps {
+  data: AutocompleteItem[];
+  isLoading: boolean;
+  onItemClick: (item: AutocompleteItem) => void;
+  openDropdown: boolean; // Add openDropdown prop
+}
+
+const DropdownComponent: React.FC<DropdownComponentProps> = ({ data, isLoading, onItemClick, openDropdown }) => {
+  return (
+    <div className={`drop_down_container ${openDropdown ? 'display' : 'display_none'}`}>
+        {isLoading && <div>Loading...</div>}
+      <ul>
+        {data?.map((item: AutocompleteItem) => (
+          <li key={item?.id} onClick={() => onItemClick(item)}>
+            {item?.name}
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default DropdownComponent;
